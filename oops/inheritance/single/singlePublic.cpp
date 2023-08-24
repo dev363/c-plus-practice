a2696509ac24233f0396c049fb27724efd1dc2c5
sat363 <sat363@gmail.com>
2023-08-24 08:24:05 +0530

    Single Inheritance with all Access Methods

--- oops/inheritance/single/singlePublic.cpp
+++ oops/inheritance/single/singlePublic.cpp
@@ -5,28 +5,39 @@ class Animal
 {
     int age = 20;
 
+protected:
+    int length = 30;
+
 public:
-    void getAnimalAge()
+    int height = 40;
+    int getAnimalPrivate()
     {
-        cout << "Animal Age: " << age << endl;
+        return age;
     }
 };
 
 class Dog : public Animal
 {
-    int age = 12;
-
 public:
-    void getDogAge()
+    int getAnimalProtected()
     {
-        cout << "Dog Age: " << age << endl;
+        return length;
     }
+    // int getAnimalPrivate2()   // Error age private
+    // {
+    //     return age + 1;
+    // }
 };
 
 int main()
 {
     Dog c1;
-    c1.getAnimalAge();
-    c1.getDogAge();
+    // cout << "Private - Animal Age:- " << c1.age << endl; // Error age private
+    cout << "Private - Animal Age:- " << c1.getAnimalPrivate() << endl;
+    // cout << "Private - Animal Age:- " << c1.getAnimalPrivate2() << endl; // Error age private
+    // cout << "Protected - Animal length:- " << c1.length << endl; // Error length protected
+    cout << "Protected - Animal length:- " << c1.getAnimalProtected() << endl;
+    cout << "Public - Animal height:- " << c1.height << endl;
+
     return 0;
 }
