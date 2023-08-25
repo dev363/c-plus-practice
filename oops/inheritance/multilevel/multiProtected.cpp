046c222533fb6842119dbbd9823e0bc4f487d1d9
sat363 <sat363@gmail.com>
2023-08-25 08:05:57 +0530

    updated multilevel public and protected

--- oops/inheritance/multilevel/multiProtected.cpp
+++ oops/inheritance/multilevel/multiProtected.cpp
@@ -46,16 +46,26 @@ class Lerbra : public Dog
 
 int main()
 {
-    LinePrint();
+    Animal A1;
+    Dog D1;
     Lerbra L1;
-    // cout << "Private - Animal Age:- " << L1.age << endl; // Error age private
-    // cout << "Protected - Animal length:- " << L1.length << endl; // Error age Protected
-    // cout << "(L1) Pubic - Animal `height`:- " << L1.height << endl; // Error public age is protected Now
+    cout << "*** Base Class Object (Animal -> A1) ****" << endl;
+    // cout << "(A1) Animal Private `age` : " << A1.age << endl;
+    // cout << "(A1) Animal Protected `length` : " << A1.length << endl;
+    cout << "(A1) Animal Public `height` : " << A1.height << endl;
+    cout << "Base Class protected and Private not access directly here" << endl;
+    LinePrint();
+
+    cout << "*** Drived Class Object (Dog -> D1) ****" << endl;
+    cout << "Base Class inherit as Protected so public members also protected now" << endl;
+    // cout << "(D1) Animal Private `age` : " << D1.age << endl;
+    // cout << "(D1) Animal Protected `length` : " << D1.length << endl;
+    // cout << "(D1) Animal Public `height` : " << D1.height << endl;
+    LinePrint();
 
     cout << "(L1) Protected - Animal `length` access by `Dog` Public Fn:- " << L1.getAnimalProtected() << endl;
     cout << "(L1) Private - Animal `age` access by `Dog` Public Fn:- " << L1.getAnimalProtectedWithDog() << endl;
     cout << "(L1) Public - Animal `age` access by `Dog` Public Fn:- " << L1.getAnimalPublicWithDog() << endl; // its Protected Now
-    // cout << "(L1) Private - Animal `age` access by `Animal` Public Fn:- " << L1.getAnimalPrivate() << endl; // its Protected Now
     LinePrint();
     return 0;
 }
--- oops/inheritance/multilevel/multiPublic.cpp
+++ oops/inheritance/multilevel/multiPublic.cpp
@@ -38,13 +38,27 @@ class Lerbra : public Dog
 
 int main()
 {
-    LinePrint();
+    Animal A1;
+    Dog D1;
     Lerbra L1;
-    // cout << "Private - Animal Age:- " << L1.age << endl; // Error age private
-    // cout << "Protected - Animal length:- " << L1.length << endl; // Error age Protected
+    cout << "*** Base Class Object (Animal -> A1) ****" << endl;
+    // cout << "(A1) Animal Private `age` : " << A1.age << endl; // Error Private
+    // cout << "(A1) Animal Protected `length` : " << A1.length << endl; // Protected access only in drived class not outside
+    cout << "(A1) Animal Public `height` : " << A1.height << endl;
+    cout << "Base Class protected and Private not access directly here" << endl;
+    LinePrint();
+
+    cout << "*** Drived Class Object (Dog -> D1) ****" << endl;
+    // cout << "Base Class inherit as Public so public members also public in drived class" << endl;
+    // cout << "(D1) Animal Private `age` : " << D1.age << endl; // Error Private
+    // cout << "(D1) Animal Protected `length` : " << D1.length << endl; // Protected access only in drived class not outside
+    cout << "(D1) Animal Public `height` : " << D1.height << endl;
+    LinePrint();
+
     cout << "(L1) Pubic - Animal `height`:- " << L1.height << endl;
     cout << "(L1) Protected - Animal `length` access by `Dog` Public Fn:- " << L1.getAnimalProtected() << endl;
     cout << "(L1) Private - Animal `age` access by `Animal` Public Fn:- " << L1.getAnimalPrivate() << endl;
     LinePrint();
+
     return 0;
 }
